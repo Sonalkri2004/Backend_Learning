9c06c5f371a74c35c565e61dc96800e796a93f40
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-12-14 19:37:30 +0530

    create a schema.

--- mongodb-basics/app.js
+++ mongodb-basics/app.js
@@ -1,4 +1,8 @@
+
+// step 1 include the mongoose module in our project
 const mongoose = require("mongoose");
+
+// step 2 connect to database
 
 mongoose
   .connect(
@@ -6,3 +10,15 @@ mongoose
   )
   .then(() => console.log("database connected successfully"))
   .catch((e) => console.log(e));
+
+
+  // step 3 make a required schema acc to our project requirement
+
+  const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number,
+    isActive: Boolean,
+    tags: [String],
+    createdAt: { type: Date, default: Date.now },
+  });
