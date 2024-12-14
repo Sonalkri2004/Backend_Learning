118e631613ba27291cc948a8d8e224661c4e9ab7
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-12-14 18:52:54 +0530

    delete a book using delete method.

--- rest-api-devlopment/app.js
+++ rest-api-devlopment/app.js
@@ -75,6 +75,26 @@ app.put("/update/:id", (req, res) => {
       });
     }
   });
+
+
+  // delete a book
+
+  app.delete("/delete/:id", (req, res) => {
+    const findIndexOfCurrentBook = books.findIndex(
+      (item) => item.id === req.params.id
+    );
+    if (findIndexOfCurrentBook !== -1) {
+      const deletedBook = books.splice(findIndexOfCurrentBook, 1);
+      res.status(200).json({
+        message: "Book deleted successfully",
+        data: deletedBook[0],
+      });
+    } else {
+      res.status(404).json({
+        message: "Book not found",
+      });
+    }
+  });
   
 
 const PORT = 3000;
