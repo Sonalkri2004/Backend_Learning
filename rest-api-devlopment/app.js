e504cd4fbe5d3144d81df6b7469c22528a88b899
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-12-14 18:26:29 +0530

    dynamic routing get book by id.

--- rest-api-devlopment/app.js
+++ rest-api-devlopment/app.js
@@ -28,6 +28,18 @@ app.get("/get", (req, res) => {
 });
 
 
+//get a single book using the concept of dynamic route
+app.get("/get/:id", (req, res) => {
+    const book = books.find((item) => item.id === req.params.id);
+    if (book) {
+      res.status(200).json(book);
+    } else {
+      res.status(404).json({
+        message: "Book not found! Please try with a different Book ID",
+      });
+    }
+  });
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is now running on port ${PORT}`);
