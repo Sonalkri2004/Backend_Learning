3708df812dad8e66b17d1d6c571a2512d9d60fa8
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-12-15 22:08:43 +0530

    import the routes in main file.

--- nodejs-auth/server.js
+++ nodejs-auth/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const connectToDB = require("./database/db");
+const authRoutes = require("./routes/auth-routes");
 
 
 const app = express();
@@ -10,6 +11,8 @@ connectToDB();
 
 //Middlewares
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is now listeining to PORT ${PORT}`);
