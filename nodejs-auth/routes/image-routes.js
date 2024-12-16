d8b256021b0e7a8dad621059f863fb8a86352e6c
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-12-16 21:27:08 +0530

    now create the route to upload the image by user.

--- nodejs-auth/routes/image-routes.js
+++ nodejs-auth/routes/image-routes.js
@@ -11,6 +11,7 @@ const {
 const router = express.Router();
 
 //upload the image
+// now we will create the route to upload the image by user for this we use our req middleware as used in the code below
 router.post(
   "/upload",
   authMiddleware,
